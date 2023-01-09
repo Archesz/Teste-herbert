fa319f67369797080aaf16f5f7bf2049406e8e48
João Vitor <jv86679@gmail.com>
2023-01-09 19:11:49 -0300

    first mobile adjust

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Menu.scss'
 
 import logo from '../../assets/logo.png'
+
+import {AiOutlineMenu} from 'react-icons/ai'
 
 window.onscroll = function(){
     let posY = window.scrollY;
@@ -9,8 +11,8 @@ window.onscroll = function(){
     
     if(posY >= 40){
         menu.style.marginTop = '0px'
-    } else if(posY < 40){
-        menu.style.marginTop = '60px'
+    } else if(posY < 60){
+        menu.style.marginTop = '80px'
     } 
 
     else if(posY >= 700 && posY < 1200){
@@ -27,21 +29,65 @@ function menuEnter(){
 }
 
 function Menu() {
+
+    const [statusMenu, setStatusMenu] = useState("hide")
+
+    function openMenu(){
+        let mobile = document.querySelector("#menu-mobile")
+        let menu = document.querySelector("#menu-mobile-items")
+        
+        if(statusMenu == "hide"){
+    
+            mobile.classList.toggle("animation-extend")
+            
+            setTimeout(() => {
+                mobile.classList.toggle("menu-extend")
+            }, 390);
+    
+            setStatusMenu("show")
+        } else{
+            mobile.classList.toggle("animation-hide")
+
+            setTimeout(() => {
+                mobile.classList.toggle("menu-hide")
+            }, 390);
+        }
+    
+    }
+
     return (
         <div className='menu-container' id='menu'>
             
-            <div className='logo'>
-                <img src={logo} alt="Logo do Projeto Herbert de Souza" className='menu-logo'/>
+            <div className='menu-padding'>
+
+                <div className='logo'>
+                    <img src={logo} alt="Logo do Projeto Herbert de Souza" className='menu-logo'/>
+                </div>
+
+                <div className='menu-items'>
+                    <span className='menu-item' onClick={menuEnter}>Home</span>
+                    <span className='menu-item' onClick={menuEnter}>História</span>
+                    <span className='menu-item' onClick={menuEnter}>Projetos</span>
+                    <span className='menu-item' onClick={menuEnter}>Contato</span>
+                    <span className='menu-item' onClick={menuEnter}>Inscrição</span>
+                </div>
+
+
+                <AiOutlineMenu className="menu-items-mobile" onClick={openMenu}/>
+
             </div>
 
-            <div className='menu-items'>
-                <span className='menu-item' onClick={menuEnter}>Home</span>
-                <span className='menu-item' onClick={menuEnter}>História</span>
-                <span className='menu-item' onClick={menuEnter}>Projetos</span>
-                <span className='menu-item' onClick={menuEnter}>Contato</span>
-                <span className='menu-item' onClick={menuEnter}>Inscrição</span>
-            </div>
+            <div className='menu-mobile' id="menu-mobile">
 
+                <div className='menu-mobile-items' id="menu-mobile-items">
+                    <span className='menu-item' onClick={menuEnter}>Home</span>
+                    <span className='menu-item' onClick={menuEnter}>História</span>
+                    <span className='menu-item' onClick={menuEnter}>Projetos</span>
+                    <span className='menu-item' onClick={menuEnter}>Contato</span>
+                    <span className='menu-item' onClick={menuEnter}>Inscrição</span>
+                </div>
+
+            </div>
         </div>
     )
 }
